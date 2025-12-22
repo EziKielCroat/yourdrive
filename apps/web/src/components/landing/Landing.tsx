ef7f7fd7c8771f48c24272a66a0d5c2b5f941c4c
Zvonk0 <117515181+Zvonk0@users.noreply.github.com>
2025-12-22 21:03:36 +0100

    Basic Landing Page Components (#1)

--- apps/web/src/components/landing/Landing.tsx
+++ apps/web/src/components/landing/Landing.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import Hero from "./components/hero/Hero";
+import Navbar_main from "../shared/navbar_main/Navbar_main";
+import Overview from "./components/overview/Overview";
+import Tryout from "./components/tryout/Tryout";
+import Footer from "../shared/footer/Footer";
+import Cta from "../shared/cta/Cta";
+import Features from "./components/features/Features";
+import Description from "./components/description/Description";
+import Faq from "./components/faq/Faq";
 
 const LandingPage: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to YourDrive</h1>
-      <p>Your secure cloud solution.</p>
-    </div>
+    <>
+      <Navbar_main type='landing'/>
+      <Hero />
+      <Overview />
+      <Tryout />
+      <Features />
+      <Description />
+      <Faq />
+      <Cta />
+      <Footer />
+    </>
   );
 };
 
---
+++ apps/web/src/components/shared/navbar_main/Navbar_main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type NavbarMainProps = {
+    type: 'landing' | 'aboutus' | 'pricing' | 'howitworks' | 'helpcenter';
+};
+
+const Navbar_main: React.FC<NavbarMainProps> = (props) => {
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Navbar_main;
